315200b9632d05a003e5d5c71d62df60b3e4e2c8
Samuel Lee <lee.sam1492@gmail.com>
2023-05-16 02:55:09 -0600

    Installed react-router. Created router.tsx. Added router provider to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
