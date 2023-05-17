5b7dc3b1c356df3ba2c580a6b08d5688621fe490
Samuel Lee <lee.sam1492@gmail.com>
2023-05-17 03:37:09 -0600

    Created navBar tsx/scss files. ImpAdded nav bar component to App.tsx. Changed themes varaiable naming. Added new theme color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./styles/App.scss";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <RouterProvider router={router} />
     </>
   );
---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,7 @@
+import "./NavBar.scss";
+
+const NavBar: React.FC = () => {
+  return <nav className="navBar"></nav>;
+};
+
+export default NavBar;
